d71ff7391ccd3bf5d9b450a1b127f34c6ec7d13b
Stuart Todd <stuarttodd444@gmail.com>
2025-01-14 13:40:05 +0000

    Coding Tips: Currying

---
+++ src/Tips/currying.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HalfShellStudios\CodingTips\Tips;
+
+/**
+ * Usage:
+ * $calculator = new Calculator();
+ * $double = $calculator->multiply(2);
+ * echo $double(5) . PHP_EOL . $double(8);
+ */
+class Calculator
+{
+    public function multiply(float $first): \Closure
+    {
+        return fn(float $second): float => $first * $second;
+    }
+}
